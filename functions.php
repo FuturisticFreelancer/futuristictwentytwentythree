a33aaf5584b6806a2b0d879c2b66aad3c4ee17eb
Kemott1991 <tomasz.burzynski.91@gmail.com>
2023-09-01 23:43:59 +0200

    Custom taxonomy for services ready. TODO: Create services, their list on front page, single service post file, archive for services category.

--- functions.php
+++ functions.php
@@ -19,6 +19,12 @@ function fttt_theme_add_styles_and_scripts(){
 
 add_action('wp_enqueue_scripts', 'fttt_theme_add_styles_and_scripts');
 
+function fttt_theme_add_admin_styles_and_scripts(){
+    wp_enqueue_style('admin_style', get_template_directory_uri().'/node_modules/bootstrap-icons/font/bootstrap-icons.css');
+}
+
+add_action('admin_enqueue_scripts', 'fttt_theme_add_admin_styles_and_scripts');
+
 function fttt_theme_register_menus(){
     register_nav_menus(
         array(
@@ -31,9 +37,9 @@ add_action('init', 'fttt_theme_register_menus');
 
 function fttt_register_taxonomies(){
     $labels = array(
-        'name' => 'Service Icons',
-        'singular_name' => 'Service Icon',
-        'menu_name' => 'Service Icons'
+        'name' => __('Service Icons'),
+        'singular_name' => __('Service Icon'),
+        'menu_name' => __('Service Icons')
     );
 
     $args = array(
@@ -50,4 +56,53 @@ function fttt_register_taxonomies(){
 }
 
 add_action('init', 'fttt_register_taxonomies');
+
+function fttt_custom_meta_boxes(){
+    add_meta_box(
+        'fttt_service_icon', 
+        __('Ikona usługi'), 
+        'fttt_service_icon_callback',
+        'post', 
+        'side', 
+        'high'
+    );
+}
+
+add_action('add_meta_boxes', 'fttt_custom_meta_boxes');
+
+function fttt_service_icon_callback($post){
+    $selected_terms = wp_get_post_terms($post->ID, 'service-icon', array('fields' => 'ids'));
+    $terms = get_terms('service-icon', array('hide_empty' => false));
+    echo '<label>'.__('Icon:').'</label><br>';
+    echo '<input type="radio" name="fttt_service_icon" value="-1" ';
+    echo empty($selected_terms) ? 'checked' : '';
+    echo ' />'.__('None').'<br>';
+    foreach($terms as $term){
+        if(!empty($selected_terms) && $term->term_id == $selected_terms[0]){
+            $checked = true;
+        }else{
+            $checked = false;
+        }
+        echo '<input type="radio" name="fttt_service_icon" value="'.$term->term_id.'" ';
+        echo $checked ? 'checked' : '';
+        echo ' /><i class="'.$term->name.'"></i> '.__('Wordpress').'<br>';
+    }
+}
+
+function save_custom_meta_box($post_id){
+    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE){
+        return;
+    }
+
+    if(!current_user_can('edit_post', $post_id)){
+        return;
+    }
+
+    if(!isset($_POST['fttt_service_icon']) && !is_array($_POST['fttt_service_icon'])){
+        return;
+    }
+
+    $selected_terms = array_map('intval', $_POST['fttt_service_icon']);
+    wp_set_post_terms($post_id, $selected_terms, 'service-icon');
+}
 ?>
